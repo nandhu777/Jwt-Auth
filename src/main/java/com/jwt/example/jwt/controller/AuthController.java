5e8d05c43250e477dd152029c12e707b7c9ff495
nandy <nandynandhulike7@gmail.com>
2025-06-29 09:29:38 +0530

    new security fix and habit table joining

--- src/main/java/com/jwt/example/jwt/controller/AuthController.java
+++ src/main/java/com/jwt/example/jwt/controller/AuthController.java
@@ -1,4 +1,5 @@
 package com.jwt.example.jwt.controller;
+import com.example.mailotp.service.MailOtpService;
 import com.jwt.example.jwt.dto.LoginRequest;
 import com.jwt.example.jwt.dto.SignupRequest;
 import com.jwt.example.jwt.enums.Role;
@@ -7,13 +8,8 @@ import com.jwt.example.jwt.repository.UserRepo;
 import com.jwt.example.jwt.security.JwtUtil;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 import java.util.Collections;
 import java.util.Optional;
 
@@ -21,17 +17,17 @@ import java.util.Optional;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-
-
+     private final MailOtpService mailOtpService;
      private final UserRepo userRepo;
      private final PasswordEncoder passwordEncoder;
      private final JwtUtil jwtUtil;
     //constructor based injection
-    public AuthController(UserRepo userRepo,PasswordEncoder passwordEncoder,JwtUtil jwtUtil)
+    public AuthController(UserRepo userRepo,PasswordEncoder passwordEncoder,JwtUtil jwtUtil,MailOtpService mailOtpService)
     {
         this.userRepo=userRepo;
         this.passwordEncoder=passwordEncoder;
         this.jwtUtil=jwtUtil;
+        this.mailOtpService=mailOtpService;
     }
 
 
@@ -40,9 +36,12 @@ public class AuthController {
         if (request.getUsername() == null && request.getEmail() == null ) {
             return ResponseEntity.badRequest().body("Username or Email cannot be empty");
         }
-        if (userRepo.findByUsername(request.getUsername()).isPresent()) {
-            return ResponseEntity.badRequest().body("Username already exists");
+        if(request.getUsername() != null){
+            if (userRepo.findByUsername(request.getUsername()).isPresent()) {
+                return ResponseEntity.badRequest().body("Username already exists");
+            }
         }
+
         if (userRepo.findByEmail(request.getEmail()).isPresent()) {
             return ResponseEntity.badRequest().body("Email already exists");
         }
@@ -55,13 +54,51 @@ public class AuthController {
         userRepo.save(user);
         return ResponseEntity.ok("User registered");
     }
+    @PostMapping({"/signup/send"})
+    public ResponseEntity<?> sendOtp(@RequestParam String email) {
+        try {
+            this.mailOtpService.generateAndSendOtp(email);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return ResponseEntity.ok("OTP has been sent to " + email);
+    }
+
+    @PostMapping({"/signup/verify"})
+    public ResponseEntity<?> verifyOtp(@RequestParam String email,@RequestParam String otp) {
+        try {
+            this.mailOtpService.verifyOtp(email, otp);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return ResponseEntity.ok("OTP has been verified ");
+    }
+
+
+
 
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest request) {
-        Optional<User> userOpt = userRepo.findByUsernameOrEmail(request.getUsername(), request.getEmail());
+
+
+        Optional<User> userOpt;
         if (request.getUsername() == null && request.getEmail() == null) {
             return ResponseEntity.badRequest().body("Username or Email is required");
         }
+        if(request.getUsername() == null){
+            userOpt=userRepo.findByEmail(request.getEmail());
+        }
+        else if(request.getEmail() == null)
+        {
+            userOpt=userRepo.findByUsername(request.getUsername());
+        }
+        else
+        {
+             userOpt = userRepo.findByUsernameOrEmail(request.getUsername(), request.getEmail());
+        }
+
+
+
         if (userOpt.isPresent() && passwordEncoder.matches(request.getPassword(), userOpt.get().getPassword())) {
             String identifier = ( userOpt.get().getUsername() != null && ! userOpt.get().getUsername().trim().isEmpty()) ?userOpt.get().getUsername() :userOpt.get().getEmail();
             String token = jwtUtil.generateToken(identifier,userOpt.get().getRole());
--- src/main/java/com/jwt/example/jwt/security/SecurityConfig.java
+++ src/main/java/com/jwt/example/jwt/security/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/auth/**").permitAll()
                         .anyRequest().authenticated()
                 )
+
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
